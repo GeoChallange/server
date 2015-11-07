a6dc7c23bd09c8b4e079b9df3ba21cd9cea9d6c7
lukas <lum@mailbox.org>
2015-11-07 23:25:04 +0100

    test

--- api.js
+++ api.js
@@ -22,6 +22,17 @@ var allowCrossDomain = function (req, res, next) {
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.all('*', function(req, res, next){
+    if (!req.get('Origin')) return next();
+    // use "*" here to accept any origin
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'PUT');
+    res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+    // res.set('Access-Control-Allow-Max-Age', 3600);
+    if ('OPTIONS' == req.method) return res.send(200);
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json());
 app.use(allowCrossDomain);
