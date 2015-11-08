a5ddca4c5bb0162243272c44a1369bcf9e3a9d45
Jasper <j.eumann@gmx.net>
2015-11-08 02:31:54 +0100

    fix put

--- api.js
+++ api.js
@@ -166,14 +166,15 @@ app.post('/challenge', function (req, res) {
 app.put('/challenge/:id', function (req, res) {
     var join = req.body;
     Log.debug(join);
-    console.log(req.params.id);
-    dbDef.Challenge.update({'_id': req.params.id}, { $push: {participants: join.userId}}, function (err, challenge) {
-        Log.debug(challenge);
-        if (err) {
-            Log.debug("can't add challenge");
-            return res.status(404).send({error: "can't add challenge"});
-        }
-        return res.status(200).send({success: true});
-    });
+    if (req.params.id != null) {
+        dbDef.Challenge.update({'_id': req.params.id}, {$push: {participants: join.userId}}, function (err, challenge) {
+            Log.debug(challenge);
+            if (err) {
+                Log.debug("can't add challenge");
+                return res.status(404).send({error: "can't add challenge"});
+            }
+            return res.status(200).send({success: true});
+        });
+    } else return res.status(404).send({error: "can't add challenge"});
 });
 
