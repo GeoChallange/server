ba2547a58536a6ceb04b0c96c68e2e50c3f9c5e9
Jasper <j.eumann@gmx.net>
2015-11-08 12:15:23 +0100

    add field winnings

--- dbDef.js
+++ dbDef.js
@@ -2,40 +2,41 @@ var Db = require('mongoose'),
     Log = require('./Log.js'),
     Config = require('./config.json'),
     challenge = Db.Schema({
-    title: String,
-    price: Number,
-    challengeId: String,
-    startLocationDescription: String,
-    startDate: Date,
-    approxDuration: String,
-    minParticipants: Number,
-    finishedBy: String,
-    participants: [
-        String
-    ],
-    quests: [
-        {
-            question: String,
-            answer: String,
-            lon: Number,
-            lat: Number
-        }
-    ],
-    pingHistory: [
-        {
-            userId: String,
-            lon: Number,
-            lat: Number,
-            timestamp: Date,
-            nextQuestIndex: Number
-        }
-    ]
-});
+        title: String,
+        price: Number,
+        winnings: String,
+        challengeId: String,
+        startLocationDescription: String,
+        startDate: Date,
+        approxDuration: String,
+        minParticipants: Number,
+        finishedBy: String,
+        participants: [
+            String
+        ],
+        quests: [
+            {
+                question: String,
+                answer: String,
+                lon: Number,
+                lat: Number
+            }
+        ],
+        pingHistory: [
+            {
+                userId: String,
+                lon: Number,
+                lat: Number,
+                timestamp: Date,
+                nextQuestIndex: Number
+            }
+        ]
+    });
 
 var Challenge = Db.model('Challenge', challenge);
 
 //Db.connect('mongodb://' + Config.dbHost + ':' + Config.dbPort +  '/geochallenge' , function(err) {
-Db.connect(process.env.MONGOLAB_URI, function(err) {
+Db.connect(process.env.MONGOLAB_URI, function (err) {
     if (err) Log.error("Database connection failed", err);
     console.log("DB runs");
 });
