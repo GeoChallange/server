14ffd39edcf8e330f4a633a37f121055864c7caf
Jasper <j.eumann@gmx.net>
2015-11-08 01:17:16 +0100

    fix put

--- api.js
+++ api.js
@@ -11,15 +11,15 @@ http.listen(app.get('port') , function () {
 });
 
 var allowCrossDomain = function (req, res, next) {
-    //res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    //next();
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Auth');
+    next();
 };
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
 app.all('*', function(req, res, next){
     if (!req.get('Origin')) return next();
     res.set('Access-Control-Allow-Origin', '*');
